19c63e437e11cdfb15519a1959f3474b2f53e17c
asusikov <susikov.alexander@gmail.com>
2017-09-04 09:45:32 +0300

    Added ability to go to component

---
+++ spec/ember-navigation-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import EmberNavigationView from '../lib/ember-navigation-view';
+
+describe('EmberNavigationView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
